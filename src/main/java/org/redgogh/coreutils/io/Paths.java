3dd3389f200df244643d2a3f01a68a04c30773e0
Red Gogh <redgogh@vip.qq.com>
2025-07-31 14:51:34 +0800

    [core/io#feat] 新增 Paths 工具类，集成 NIO Paths 以及自定义 Path 工具方法

---
+++ src/main/java/org/redgogh/coreutils/io/Paths.java
@@ -0,0 +1,69 @@
+package org.redgogh.coreutils.io;
+
+import org.redgogh.coreutils.system.SystemUtils;
+
+/* -------------------------------------------------------------------------------- *\
+|*                                                                                  *|
+|*    Copyright (C) 2019-2024 RedGogh All rights reserved.                          *|
+|*                                                                                  *|
+|*    Licensed under the Apache License, Version 2.0 (the "License");               *|
+|*    you may not use this file except in compliance with the License.              *|
+|*    You may obtain a copy of the License at                                       *|
+|*                                                                                  *|
+|*        http://www.apache.org/licenses/LICENSE-2.0                                *|
+|*                                                                                  *|
+|*    Unless required by applicable law or agreed to in writing, software           *|
+|*    distributed under the License is distributed on an "AS IS" BASIS,             *|
+|*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.      *|
+|*    See the License for the specific language governing permissions and           *|
+|*    limitations under the License.                                                *|
+|*                                                                                  *|
+\* -------------------------------------------------------------------------------- */
+
+/* Creates on 2025/7/31. */
+
+import java.net.URI;
+import java.nio.file.Path;
+
+/**
+ * 路径相关的工具方法
+ *
+ * @author Red Gogh
+ */
+public class Paths {
+
+    /**
+     * 根据路径字符串构造 Path 对象
+     *
+     * @param first 路径的第一个部分（不能为null）
+     * @param more 路径的额外部分（可选）
+     * @return 结果 Path 对象
+     * @see java.nio.file.Paths#get(String, String...)
+     */
+    public static Path get(String first, String... more) {
+        return java.nio.file.Paths.get(first, more);
+    }
+
+    /**
+     * 根据 URI 构造 Path 对象
+     *
+     * @param uri 要转换的 URI（不能为null）
+     * @return 结果 Path 对象
+     * @see java.nio.file.Paths#get(URI)
+     */
+    public static Path get(URI uri) {
+        return java.nio.file.Paths.get(uri);
+    }
+
+    /**
+     * 解析路径字符串（可能处理特殊符号如 ~ 表示用户目录）
+     *
+     * @param pathname 要解析的路径字符串
+     * @return 解析后的绝对路径字符串
+     * @see SystemUtils#resolvePath(String)
+     */
+    public static String resolve(String pathname) {
+        return SystemUtils.resolvePath(pathname);
+    }
+
+}
